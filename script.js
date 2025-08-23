b35c8be2dfeee4372c8b1bf2494574612ce9d483
Diogo <santos.vieira.diogo@escola.pr.gov.br>
2025-08-23 11:26:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -123,5 +123,11 @@ function renderFallback({subjects, hours, goal}){
 }
 
 function escapeHtml(s){
-  return String(s).replace(/[&<>"']/g, (m)=>({ "&":"&amp;","<":"&lt;",">":"&gt;",""":"&quot;","'":"&#039;" }[m]));
+  return String(s).replace(/[&<>"']/g, (m)=>({
+    "&":"&amp;",
+    "<":"&lt;",
+    ">":"&gt;",
+    "\"":"&quot;",
+    "'":"&#039;"
+  }[m]));
 }
